56a5f6fb79657bd1794089d16c0757f7c89c16e8
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-12 16:01:15 +0200

    extract assert_equals

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@
 use std::process::exit;
 
 fn main() {
-    let mut failures: Vec<&str> = Vec::new();
+    let mut failures: Vec<String> = Vec::new();
 
-    assert_true(true, &mut failures);
+    assert_true(false, &mut failures);
 
     test_outcome(failures);
 }
 
-fn assert_true(value: bool, failures: &mut Vec<&str>) {
-    if value != true {
-        failures.push("Value not true");
+fn assert_true(value: bool, failures: &mut Vec<String>) {
+    assert_equals(value, true, failures);
+}
+
+fn assert_equals(actual: bool, expected: bool, failures: &mut Vec<String>) {
+    if actual != expected {
+        failures.push(format!("Expected '{}' but got '{}'", expected, actual));
     }
 }
 
-fn test_outcome(failures: Vec<&str>) {
+fn test_outcome(failures: Vec<String>) {
     if !failures.is_empty() {
         for failure in &failures {
             println!("{}", failure)
