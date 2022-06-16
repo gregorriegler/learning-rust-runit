3a834fc5fc93777e1d6adab28c458e8c5ec5eae0
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-16 23:37:16 +0200

    print test names

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@
 use std::panic;
 use std::process::exit;
 
-fn main() {
-    let mut results: Vec<std::thread::Result<()>> = Vec::new();
+type TestCaseRun = (&'static str, std::thread::Result<()>);
 
-    add_test(first_test, &mut results);
+fn main() {
+    let mut results: Vec<TestCaseRun> = Vec::new();
+
+    add_test("first test", first_test, &mut results);
 
     verify(results);
 }
 
-fn add_test(test_fn: fn(), results: &mut Vec<std::thread::Result<()>>) {
+fn add_test(name_of_test: &'static str, test_fn: fn(), results: &mut Vec<TestCaseRun>) {
     let test_1_result = panic::catch_unwind(|| {
         test_fn();
     });
-    results.push(test_1_result);
+    results.push((name_of_test, test_1_result));
 }
 
 fn first_test() {
@@ -24,15 +26,15 @@ fn first_test() {
     assert_true(false);
 }
 
-fn verify(results: Vec<std::thread::Result<()>>) {
+fn verify(results: Vec<TestCaseRun>) {
     if !results.is_empty() {
         println!("Test Results:");
         let mut failure: bool = false;
-        for res in &results {
-            // todo print testname
-            match res {
+        for run in &results {
+            let (test_name, result) = run;
+            match result {
                 Ok(_) => {
-                    println!("Test Ok");
+                    println!("{} successful", test_name);
                 }
                 Err(e) => {
                     let msg = if let Some(msg) = e.downcast_ref::<String>() {
@@ -40,16 +42,18 @@ fn verify(results: Vec<std::thread::Result<()>>) {
                     } else {
                         format!("?{:?}", e)
                     };
-                    println!("{}", msg);
+                    println!("{} failed with reason: {}", test_name, msg);
                     failure = true
                 }
             }
         }
         if failure {
-            test_failure();
+            println!("Test Failure!");
+            exit(1)
+        } else {
+            println!("Test Success!");
         }
     }
-    println!("Test Success!");
 }
 
 
@@ -61,9 +65,4 @@ fn assert_equals(actual: bool, expected: bool) {
     if actual != expected {
         panic!("{}", format!("Expected '{}' but got '{}'", expected, actual))
     }
-}
-
-fn test_failure() -> ! {
-    println!("Test Failure!");
-    exit(1)
 }
