221b3c4e41d0260c71c01fa1f592c6e60930be30
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-16 23:54:02 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ fn main() {
     run(tests)
 }
 
-fn add_test(name_of_test: &'static str, test_fn: fn(), results: &mut Vec<TestCase>) {
-    results.push((name_of_test, test_fn));
-}
-
 fn successful_test() {
     assert_true(true);
 }
@@ -26,23 +22,21 @@ fn failing_test() {
     assert_true(false);
 }
 
+fn add_test(name_of_test: &'static str, test_fn: fn(), results: &mut Vec<TestCase>) {
+    results.push((name_of_test, test_fn));
+}
+
 fn run(tests: Vec<TestCase>) {
     if !tests.is_empty() {
         println!("Test Results:");
 
-        let mut failure: bool = false;
+        let mut success: bool = true;
 
         for test in &tests {
             let (test_name, test_fn) = test;
 
-            let test_result = panic::catch_unwind(|| {
-                test_fn();
-            });
-
-            match test_result {
-                Ok(_) => {
-                    println!("{} successful", test_name);
-                }
+            match panic::catch_unwind(|| { test_fn(); }) {
+                Ok(_) => println!("{} successful", test_name),
                 Err(e) => {
                     let msg = if let Some(msg) = e.downcast_ref::<String>() {
                         msg.clone()
@@ -50,17 +44,21 @@ fn run(tests: Vec<TestCase>) {
                         format!("?{:?}", e)
                     };
                     println!("{} failed with reason: {}", test_name, msg);
-                    failure = true
+                    success = false
                 }
             }
         }
-        if failure {
-            println!("Test Failure!");
-            exit(1)
-        } else {
-            println!("Test Success!");
-        }
+
+        successOrFailure(success)
     }
+}
+
+fn successOrFailure(success: bool) {
+    if !success {
+        println!("Test Failure!");
+        exit(1)
+    }
+    println!("Test Success!");
 }
 
 fn assert_true(value: bool) {
