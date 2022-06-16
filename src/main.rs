73a56966fe083085958c6157cfb0fff13fa4d392
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-16 22:06:02 +0200

    collect Results

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,22 @@ use std::process::exit;
 
 fn main() {
     let mut results: Vec<std::thread::Result<()>> = Vec::new();
+
     let test_1_result = panic::catch_unwind(|| {
         test_function();
     });
 
     results.push(test_1_result);
 
+    test_outcome(results);
+}
+
+fn test_outcome(results: Vec<std::thread::Result<()>>) {
     if !results.is_empty() {
         println!("Test Results:");
         let mut failure: bool = false;
         for res in &results {
+            // todo print testname
             println!("{:?}", res); // todo prints Any { .. }
             if res.is_err() {
                 failure = true;
