3e5d826045ddfdea23ba601406a0b96065435602
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-16 23:48:15 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,42 @@
 use std::panic;
 use std::process::exit;
 
-type TestCaseRun = (&'static str, std::thread::Result<()>);
+type TestCase = (&'static str, fn());
 
 fn main() {
-    let mut results: Vec<TestCaseRun> = Vec::new();
-
-    run_test("first test", first_test, &mut results);
-
-    verify(results);
+    let mut tests: Vec<TestCase> = Vec::new();
+    add_test("successful test", successful_test, &mut tests);
+    add_test("failing test", failing_test, &mut tests);
+    run(tests)
 }
 
-fn run_test(name_of_test: &'static str, test_fn: fn(), results: &mut Vec<TestCaseRun>) {
-    let test_1_result = panic::catch_unwind(|| {
-        test_fn();
-    });
-    results.push((name_of_test, test_1_result));
+fn add_test(name_of_test: &'static str, test_fn: fn(), results: &mut Vec<TestCase>) {
+    results.push((name_of_test, test_fn));
 }
 
-fn first_test() {
+fn successful_test() {
+    assert_true(true);
+}
+
+fn failing_test() {
     assert_true(true);
     assert_true(false);
 }
 
-fn verify(results: Vec<TestCaseRun>) {
-    if !results.is_empty() {
+fn run(tests: Vec<TestCase>) {
+    if !tests.is_empty() {
         println!("Test Results:");
+
         let mut failure: bool = false;
-        for run in &results {
-            let (test_name, result) = run;
-            match result {
+
+        for test in &tests {
+            let (test_name, test_fn) = test;
+
+            let test_result = panic::catch_unwind(|| {
+                test_fn();
+            });
+
+            match test_result {
                 Ok(_) => {
                     println!("{} successful", test_name);
                 }
@@ -55,7 +62,6 @@ fn verify(results: Vec<TestCaseRun>) {
         }
     }
 }
-
 
 fn assert_true(value: bool) {
     assert_equals(value, true);
