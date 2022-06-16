3ba3080060f1aec395fc420993ce286be32e9cbd
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-16 21:20:01 +0200

    use panic

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 // assert true for true -> success, programm does not fail
 // assert true for false -> failure
 
+use std::panic;
 use std::process::exit;
 
 fn main() {
     let mut failures: Vec<String> = Vec::new();
+    let result = panic::catch_unwind(|| {
+        test_function();
+    });
 
-    assert_true(false, &mut failures);
+    match result {
+        Ok(ok) => ok,
+        Err(err) => failures.push(format!("test failed with because: {:?}", err)) // TODO prints Any { .. }
+    }
 
-    test_outcome(failures);
+    test_outcome(failures)
 }
 
-fn assert_true(value: bool, failures: &mut Vec<String>) {
-    assert_equals(value, true, failures);
+fn test_function() {
+    assert_true(true);
+    assert_true(false);
 }
 
-fn assert_equals(actual: bool, expected: bool, failures: &mut Vec<String>) {
+fn assert_true(value: bool) {
+    assert_equals(value, true);
+}
+
+fn assert_equals(actual: bool, expected: bool) {
     if actual != expected {
-        failures.push(format!("Expected '{}' but got '{}'", expected, actual));
+        panic!("{}", format!("Expected '{}' but got '{}'", expected, actual))
     }
 }
 
