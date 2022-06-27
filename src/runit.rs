9bb4f2616844a6d6586fcc3cbf3bfa5699f99096
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-27 23:09:11 +0200

    wip

--- src/runit.rs
+++ src/runit.rs
@@ -3,36 +3,27 @@ use std::process::exit;
 use crate::runit::TestCaseOutcome::{Fail, Pass};
 
 pub type TestCase = (&'static str, fn());
-pub type TestCaseResult = (&'static str, TestCaseOutcome);
-
-pub fn successful_case(name: &'static str) -> TestCaseResult {
-    return (name, Pass);
-}
-
-pub fn failing_case(name: &'static str, reason: &'static str) -> TestCaseResult {
-    return (name, Fail(reason));
-}
 
 pub struct TestSuite {
     tests: Vec<TestCase>,
 }
 
-pub struct TestCaseResultNew {
+pub struct TestCaseResult {
     name: &'static str,
     outcome: TestCaseOutcome
 }
 
-impl TestCaseResultNew {
+impl TestCaseResult {
 
-    pub fn pass(name: &'static str) -> TestCaseResultNew {
-        return TestCaseResultNew {
+    pub fn pass(name: &'static str) -> TestCaseResult {
+        return TestCaseResult {
             name,
             outcome: Pass
         }
     }
 
-    pub fn fail(name: &'static str, reason: &'static str) -> TestCaseResultNew {
-        return TestCaseResultNew {
+    pub fn fail(name: &'static str, reason: &'static str) -> TestCaseResult {
+        return TestCaseResult {
             name,
             outcome: Fail(reason)
         }
@@ -71,7 +62,7 @@ impl TestSuite {
     }
 
     // TODO: all prints go to printer
-    fn run_with_printer(self, print: &dyn Fn(&Vec<TestCaseResultNew>) -> ()) {
+    fn run_with_printer(self, print: &dyn Fn(&Vec<TestCaseResult>) -> ()) {
         if self.tests.is_empty() {
             println!("No Tests to run");
             return;
@@ -91,14 +82,12 @@ impl TestSuite {
         success_or_failure(success)
     }
 
-    fn run_cases(self) -> Vec<TestCaseResultNew> {
+    fn run_cases(self) -> Vec<TestCaseResult> {
         let mut results: Vec<TestCaseResult> = Vec::new();
-        let mut results_new: Vec<TestCaseResultNew> = Vec::new();
         for (test_name, test_fn) in &self.tests {
             match panic::catch_unwind(|| test_fn()) {
                 Ok(_) => {
-                    results.push(successful_case(test_name));
-                    results_new.push(TestCaseResultNew::pass(test_name))
+                    results.push(TestCaseResult::pass(test_name))
                 }
                 Err(e) => {
                     let msg = if let Some(msg) = e.downcast_ref::<String>() {
@@ -107,15 +96,14 @@ impl TestSuite {
                         format!("?{:?}", e)
                     };
                     let static_msg = Box::leak(msg.into_boxed_str());
-                    results.push(failing_case(test_name, static_msg));
-                    results_new.push(TestCaseResultNew::fail(test_name, static_msg))
+                    results.push(TestCaseResult::fail(test_name, static_msg))
                 }
             }
         }
-        results_new
+        results
     }
 
-    fn simple_print(results: &Vec<TestCaseResultNew>) {
+    fn simple_print(results: &Vec<TestCaseResult>) {
         for result in results {
             match result.outcome {
                 Pass => {
