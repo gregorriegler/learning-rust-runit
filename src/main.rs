97680e6a51c38f97c010f4667ec257804b55067d
Gregor Riegler <gregor.riegler@ebcont.com>
2022-06-29 20:40:43 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,24 @@ use crate::runit::simple_print::simple_print;
 mod runit;
 
 fn main() {
-    suite(
-        "Outer Suite",
-        &[
-            describe("Inner Suite 1", &[
-                it(
-                    "successful test",
-                    || assert_true(true),
-                )
-            ]),
-            describe("Inner Suite 2", &[
-                it(
-                    "successful test",
-                    || assert_true(true),
-                ),
-                it(
-                    "failing test2",
-                    || assert_true(false),
-                ),
-            ])
-        ],
+    suite("Outer Suite", &[
+        describe("Inner Suite 1", &[
+            it(
+                "successful test",
+                || assert_true(true),
+            )
+        ]),
+        describe("Inner Suite 2", &[
+            it(
+                "successful test",
+                || assert_true(true),
+            ),
+            it(
+                "failing test2",
+                || assert_true(false),
+            ),
+        ])
+    ],
     ).run()
         .print(simple_print)
         .exit_on_failure();
